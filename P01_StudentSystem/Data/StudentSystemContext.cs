9cc7828b8afd27c64f424871d1e906a5562e4fe3
AmmarMKamel <ammarkamel9899@gmail.com>
2025-09-22 05:10:02 +0300

    Create the DbContext

---
+++ P01_StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P01_StudentSystem.Data
+{
+    internal class StudentSystemContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=StudentSystem;Trusted_Connection=True;");
+        }
+    }
+}
